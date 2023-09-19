01009b3a64fa4ca600d99cafa9a4936a67ebf6d2
Ilori Jesugbemisola <ifeoluwah4real@gmail.com>
2023-09-19 13:17:03 +0100

    initial commit

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const mobileNav = document.querySelector("nav.mobile-nav");
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
 const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+const addToCart = document.querySelector(".addToCart");
+const cart = document.getElementsByClassName("dashboard-order");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 60) {
